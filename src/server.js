b109549d8b7a2a711295bbcdfca710cab0225062
ADITYA PATIL <adipatil@ptc.com>
2021-04-21 17:45:27 +0530

    uncommenting static files loading code

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import ArticleService from './article-services';
 
 const app = express();
 app.use(bodyParser.json());
-// app.use(express.static(path.join(__dirname, '/build')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 const articleService = new ArticleService();
 
@@ -184,8 +184,8 @@ app.post('/api/article/:name/add-comment', async (req, res) => {
     }, res);
 });
 
-// app.get('*', (req, res) => {
-//     res.sendFile(path.join(__dirname + '/build/index.html'));
-// });
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+});
 
 app.listen('3010', () => console.log('listening port 3010!'));
